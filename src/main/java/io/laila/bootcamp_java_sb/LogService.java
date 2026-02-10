bbfbfaea03ab6ba7a7b38b8ff7e778c8bddc7790
lailatulbadriyah24 <lailatulbadriyah654@gmail.com>
2026-02-10 09:33:15 +0700

    Remove commented line for clarity in LogService

--- src/main/java/io/laila/bootcamp_java_sb/LogService.java
+++ src/main/java/io/laila/bootcamp_java_sb/LogService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class LogService {
 
-    // Banyak logger untuk testing multi file
     private static final Logger loggerA = LoggerFactory.getLogger("LOGGER_A");
     private static final Logger loggerB = LoggerFactory.getLogger("LOGGER_B");
     private static final Logger loggerC = LoggerFactory.getLogger("LOGGER_C");
